40c6d3cbe2b0312dcf0429985570d14f1f127067
duanbaihhu <duanbaihu>
2020-05-29 18:35:20 +0800

    add

--- apps/static/js/inspinia.js
+++ apps/static/js/inspinia.js
@@ -41,7 +41,6 @@ $(document).ready(function () {
     // minimalize menu
     $('.navbar-minimalize').click(function () {
         $("body").toggleClass("mini-navbar");
-        SmoothlyMenu();
     })
 
     // tooltips
@@ -107,27 +106,6 @@ $(function() {
         }
     })
 })
-
-function SmoothlyMenu() {
-    if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
-        // Hide menu in order to smoothly turn on when maximize menu
-        $('#side-menu').hide();
-        // For smoothly turn on menu
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(500);
-            }, 100);
-    } else if ($('body').hasClass('fixed-sidebar')){
-        $('#side-menu').hide();
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(500);
-            }, 300);
-    } else {
-        // Remove all inline style from jquery fadeIn function to reset menu state
-        $('#side-menu').removeAttr('style');
-    }
-}
 
 // Dragable panels
 function WinMove() {
